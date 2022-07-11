3383b8953a97c8e5debf939f11435d8ac48e885b
Anastasia <anastasijatsar@mail.ru>
2022-07-11 12:03:57 +0300

    Начинаем программировать

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const Setting = {
+  PLACES_COUNT: 312,
+};
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      placesCount = {Setting.PLACES_COUNT}
+    />
   </React.StrictMode>,
 );

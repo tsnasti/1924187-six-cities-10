762b7b704f72eebad8a0f46927498aebcf8a78b2
Anastasia <anastasijatsar@mail.ru>
2022-07-13 14:58:14 +0300

    Начинаем программировать

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,11 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import {AppRoute, AuthorizationStatus} from '../../const';
 import MainPage from '../../pages/main-page/main-page';
+import LoginPage from '../../pages/login-page/login-page';
+import FavoritesPage from '../../pages/favorites-page/favorites-page';
+import PropertyPage from '../../pages/property-page/property-page';
+import ErrorPage from '../../pages/error-page/error-page';
+import PrivateRoute from '../private-route/private-route';
 
 type AppScreenProps = {
   placesCount: number;
@@ -6,7 +13,36 @@ type AppScreenProps = {
 
 function App({placesCount}: AppScreenProps): JSX.Element {
   return (
-    <MainPage placesCount = {placesCount} />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={<MainPage placesCount = {placesCount} />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<LoginPage />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={
+            <PrivateRoute
+              authorizationStatus={AuthorizationStatus.NoAuth}
+            >
+              <FavoritesPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path={AppRoute.Room}
+          element={<PropertyPage />}
+        />
+        <Route
+          path="*"
+          element={<ErrorPage />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
